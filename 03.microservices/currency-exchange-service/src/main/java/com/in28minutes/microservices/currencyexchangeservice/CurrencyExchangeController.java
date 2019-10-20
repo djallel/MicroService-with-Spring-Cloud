aa76847b6cfb695ee52f655bb4f149cef5994ad4
TABET Djalal <b_tabet@esi.dz>
2019-10-20 17:56:51 +0200

    step 44

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private Environment environment;
 	
@@ -26,6 +28,8 @@ public class CurrencyExchangeController {
 		
 		exchangeValue.setPort(
 				Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("{}", exchangeValue);
 		
 		return exchangeValue;
 	}
